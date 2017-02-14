33a014a57bec6797872a6fd0fc24f1527545c18f
rodrigobell <rbell594@gmail.com>
2017-02-13 20:03:14 -0800

    Added basic cell structure, followed video one from CodePath to completion

---
+++ Yelp/BusinessCell.swift
@@ -0,0 +1,46 @@
+//
+//  BusinessCell.swift
+//  Yelp
+//
+//  Created by Rodrigo Bell on 2/13/17.
+//  Copyright © 2017 Rodrigo Bell. All rights reserved.
+//
+
+import UIKit
+
+class BusinessCell: UITableViewCell {
+
+    @IBOutlet weak var thumbImageView: UIImageView!
+    @IBOutlet weak var ratingsImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var distanceLabel: UILabel!
+    @IBOutlet weak var reviewsCountLabel: UILabel!
+    @IBOutlet weak var addressLabel: UILabel!
+    @IBOutlet weak var categoriesLabel: UILabel!
+    
+    var business: Business! {
+        didSet {
+            thumbImageView.setImageWith(business.imageURL!)
+            ratingsImageView.setImageWith(business.ratingImageURL!)
+            nameLabel.text = business.name
+            distanceLabel.text = business.distance
+            reviewsCountLabel.text = "\(business.reviewCount!) Reviews"
+            addressLabel.text = business.address
+            categoriesLabel.text = business.categories
+        }
+    }
+    
+    override func awakeFromNib() {
+        thumbImageView.layer.cornerRadius = 3
+        thumbImageView.clipsToBounds = true
+    }
+    
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
--- Yelp/BusinessesViewController.swift
+++ Yelp/BusinessesViewController.swift
@@ -8,16 +8,23 @@
 
 import UIKit
 
-class BusinessesViewController: UIViewController {
+class BusinessesViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     var businesses: [Business]!
+    
+    @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        tableView.delegate = self
+        tableView.dataSource = self
+        
         Business.searchWithTerm(term: "Thai", completion: { (businesses: [Business]?, error: Error?) -> Void in
             
             self.businesses = businesses
+            self.tableView.reloadData()
+            
             if let businesses = businesses {
                 for business in businesses {
                     print(business.name!)
@@ -27,24 +34,30 @@ class BusinessesViewController: UIViewController {
             
             }
         )
-        
-        /* Example of Yelp search with more search options specified
-         Business.searchWithTerm("Restaurants", sort: .Distance, categories: ["asianfusion", "burgers"], deals: true) { (businesses: [Business]!, error: NSError!) -> Void in
-         self.businesses = businesses
-         
-         for business in businesses {
-         print(business.name!)
-         print(business.address!)
-         }
-         }
-         */
-        
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if businesses != nil {
+            return businesses!.count
+        } else {
+            return 0
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "business-cell", for: indexPath) as! BusinessCell
+        
+        cell.business = businesses[indexPath.row]
+        cell.selectionStyle = .none
+        
+        return cell
+    }
+    
     
     /*
      // MARK: - Navigation
